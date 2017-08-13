8d8534ff5157fbae9e382f1fa0528f6486e667a4
Chris Lowe <clowe@luc.edu>
2017-08-12 22:50:57 -0500

    Start re-architecture

---
+++ FileUtil.Service/ServiceInterfaces/FindDuplicateFilesService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Configuration;
+
+namespace FileUtil.Service.ServiceInterfaces
+{
+	public interface IFindDuplicateFilesService
+	{
+		void FindDuplicates();
+	}
+
+	public class FindDuplicateFilesService
+	{
+		public void FindDuplicates()
+		{
+			//Read app config options or //todo console selections
+			//Create new FDJ object 
+			FindDuplicatesJob job = new FindDuplicatesJob(SafeGetAppConfigs());
+			//Call into core
+		}
+
+		private NameValueCollection SafeGetAppConfigs()
+		{
+			NameValueCollection appSettings = new NameValueCollection();
+			try
+			{
+				appSettings = ConfigurationManager.AppSettings;
+			}
+			catch (ConfigurationErrorsException ex)
+			{
+				Console.WriteLine($"Unable to read from configurations. {ex.Message}");
+			}
+
+			if (appSettings == null || appSettings.Count == 0)
+			{
+				throw new System.ArgumentException("Unable to retrieve configuration values from the app.config file. ");
+			}
+
+			return appSettings;
+		}
+	}
+}
