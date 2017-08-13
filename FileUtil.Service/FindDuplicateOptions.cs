1ede64e2faeb4ad34a4465f0836c36ca967f9f3d
Chris Lowe <clowe@luc.edu>
2017-08-12 22:50:57 -0500

    Start re-architecture

---
+++ FileUtil.Service/FindDuplicateOptions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileUtil.Service
+{
+	public class FindDuplicateOptions
+	{
+		public bool IsLocalFileSystem;
+		public string User;
+		public string Pass;
+		public string Domain;
+		public string Path;
+		public int HashLimit;
+
+		public FindDuplicateOptions(NameValueCollection appSettings)
+		{
+			Boolean.TryParse(appSettings["isLocalFileSystem"], out IsLocalFileSystem);
+			User = appSettings["NetworkShareUser"] ?? "User Not Found";
+			Pass = appSettings["NetworkSharePassword"] ?? "Password Not Found";
+			Domain = appSettings["NetworkShareDomain"] ?? "Domain Not Found";
+			Path = appSettings["NetworkShareUncPath"] ?? "UNC Path Not Found";
+			Int32.TryParse(appSettings["NetworkShareUncPath"], out HashLimit);
+		}
+	}
+}
---
+++ FileUtil.Service/FindDuplicatesJob.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileUtil.Service
+{
+	public class FindDuplicatesJob
+	{
+		public string Path;
+		public string[] filesArr;
+		public FindDuplicateOptions Options;
+		private NameValueCollection _appSettings;
+
+		public FindDuplicatesJob(NameValueCollection appSettings)
+		{
+			this._appSettings = appSettings;
+			Options = new FindDuplicateOptions(appSettings);
+		}
+
+
+	}
+}
