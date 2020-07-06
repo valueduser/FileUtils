1a97dd6635b672142a7974c8e347c25c59afb133
Chris Lowe <clowe@luc.edu>
2020-07-05 21:09:42 -0600

    Migrate to .Net Core 3.1

--- FileUtil.Data/Class1.cs
+++ FileUtil.Data/Class1.cs
@@ -1,12 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FileUtil.Data
 {
-    public class Class1
-    {
-    }
+	public class Class1
+	{
+	}
 }
